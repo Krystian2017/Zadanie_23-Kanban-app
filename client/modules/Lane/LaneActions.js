aabf9c50b0fab283c06fbb77c085a8f49861be1f
Krystian Bieniek <krystekk2013@gmail.com>
2018-09-03 14:31:05 +0200

    Add actions client

--- client/modules/Lane/LaneActions.js
+++ client/modules/Lane/LaneActions.js
@@ -1,32 +1,32 @@
-// import uuid from 'uuid';
+import uuid from 'uuid';
 
-// // Export Constants
-// export const CREATE_LANE = 'CREATE_LANE';
-// export const UPDATE_LANE = 'UPDATE_LANE';
-// export const DELETE_LANE = 'DELETE_LANE';
+// Export Constants
+export const CREATE_LANE = 'CREATE_LANE';
+export const UPDATE_LANE = 'UPDATE_LANE';
+export const DELETE_LANE = 'DELETE_LANE';
 
-// // Export Actions
-// export function createLane(lane) {
-//   return {
-//     type: CREATE_LANE,
-//     lane: {
-//       id: uuid(),
-//       notes: [],
-//       ...lane,
-//     }
-//   };
-// }
+// Export Actions
+export function createLane(lane) {
+  return {
+    type: CREATE_LANE,
+    lane: {
+      id: uuid(),
+      notes: [],
+      ...lane,
+    }
+  };
+}
 
-// export function updateLane(lane) {
-//   return {
-//     type: UPDATE_LANE,
-//     lane,
-//   };
-// }
+export function updateLane(lane) {
+  return {
+    type: UPDATE_LANE,
+    lane,
+  };
+}
 
-// export function deleteLane(laneId) {
-//   return {
-//     type: DELETE_LANE,
-//     laneId
-//   };
-// }
+export function deleteLane(laneId) {
+  return {
+    type: DELETE_LANE,
+    laneId
+  };
+}
