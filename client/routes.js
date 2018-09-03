9c9febef0f24b19ce98edcc8314ef46e83bd9337
Krystian Bieniek <krystekk2013@gmail.com>
2018-09-03 16:54:01 +0200

    Add components-containers in client

--- client/routes.js
+++ client/routes.js
@@ -12,8 +12,7 @@ if (typeof require.ensure !== 'function') {
 
 if (process.env.NODE_ENV !== 'production') {
   // Require async routes only in development for react-hot-reloader to work.
-  require('./modules/Post/pages/PostListPage/PostListPage');
-  require('./modules/Post/pages/PostDetailPage/PostDetailPage');
+  require('./modules/Kanban/Kanban');
 }
 
 export default (
@@ -21,7 +20,7 @@ export default (
     <IndexRoute
       getComponent={(nextState, cb) => {
         require.ensure([], require => {
-          cb(null, require('./modules/Post/pages/PostListPage/PostListPage').default);
+          cb(null, require('./modules/Kanban/Kanban').default);
         });
       }}
     />
