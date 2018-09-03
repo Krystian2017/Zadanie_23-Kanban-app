3373f1d30c2318f70ec860a6f1951396c61dd58b
Krystian Bieniek <krystekk2013@gmail.com>
2018-09-03 20:42:53 +0200

    Add server

--- server/models/about.js
+++
@@ -1,8 +0,0 @@
-import mongoose from 'mongoose';
-const Schema = mongoose.Schema;
-
-const aboutSchema = new Schema({
-
-});
-
-export default mongoose.model('About', aboutSchema);
--- server/routes/about.routes.js
+++
@@ -1,7 +0,0 @@
-import { Router } from 'express';
-import * as AboutController from '../controllers/about.controller';
-
-const router = new Router();
-
-
-export default router;
